c01fb4e9ed62a677dcfa8426fc295f31adfff02a
Joe Benga <jbenga.iii@gmail.com>
2019-04-10 10:57:51 -0700

    Added File copy and ros install run

--- 06 install.tf
+++ 06 install.tf
@@ -1,9 +1,28 @@
-resource "null_resource" "cloud-config" {
+resource "null_resource" "cloudconfig-cp" {
+  count = 2
+
+  connection {
+      type = "ssh"
+      host = "${vcd_vapp_vm.cp.*.ip}"
+  }
+
   provisioner "local-exec" {
       command = <<EOT
-      touch cloud-config.yml;
-      echo "ssh_authorized_keys:" > cloud-config.yml;
-      echo "  - ssh-rsa ${var.ssh_rsa}" >> cloud-config.yml
+      touch "${var.node_cp}-${count.index}".yml;
+      echo "ssh_authorized_keys:" > "${var.node_cp}-${count.index}".yml;
+      echo "  - ssh-rsa ${var.ssh_rsa}" >> "${var.node_cp}-${count.index}".yml;
+      echo "hostname: ${var.node_cp}-${count.index}" >> "${var.node_cp}-${count.index}".yml
       EOT
+  }
+
+  provisioner "file" {
+      source = "${var.node_cp}-${count.index}.yml"
+      destination = "/home/rancher/cloud-config.yml"
+  }
+
+  provisioner "remote-exec"{
+      inline = [
+          "sudo ros install cloud-config.yml -f"
+      ]      
   }
 }
