58ffa3744913e4f2df1130d727c3c14a987b99da
Joe Benga <jbenga.iii@gmail.com>
2019-04-18 11:53:30 -0700

    fix the concat list of resources to ips

--- 04 output.tf
+++ 04 output.tf
@@ -7,17 +7,17 @@ resource "null_resource" "delay" {
 }
 
 output "cp" {
-  value = "${concat(list("${vcd_vapp_vm.cp.*.name}"), list("${vcd_vapp_vm.cp.*.ip}"))}"
+  value = "${concat(vcd_vapp_vm.cp.*.name, vcd_vapp_vm.cp.*.ip)}"
 
   depends_on = ["null_resource.delay"]
 }
 output "etcd" {
-  value = "${concat(list("${vcd_vapp_vm.etcd.*.name}"), list("${vcd_vapp_vm.etcd.*.ip}"))}"
+  value = "${concat(vcd_vapp_vm.etcd.*.name, vcd_vapp_vm.etcd.*.ip)}"
 
   depends_on = ["null_resource.delay"]
 }
 output "worker" {
-  value = "${concat(list("${vcd_vapp_vm.work.*.name}"), list("${vcd_vapp_vm.work.*.ip}"))}"
+  value = "${concat(vcd_vapp_vm.work.*.name, vcd_vapp_vm.work.*.ip)}"
 
   depends_on = ["null_resource.delay"]
 }
